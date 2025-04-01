d1d2eea6bf4fcfa5feb72be59b6d900ae6b025a9
liakh83 <lahn4573@gmail.com>
2025-04-01 16:27:46 +0300

    feat: add switch between Garage and Winners pages

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,17 +1,25 @@
 import '@/app/components/header/header.scss';
 import { createButton } from '@/app/components/button/button';
 import { createElement } from '@/app/utils/create-element';
+import { appMain } from '@/app/app';
+import { optionSection } from '@/app/pages/garage/page-garage';
+import { winnerSection } from '@/app/pages/winner/winner-section';
 
 const toGarageBtn = createButton({
   textContent: 'To Garage',
+  onclick: () => {
+    appMain.replaceChildren(optionSection);
+  },
 });
 
 const toWinnerBtn = createButton({
   textContent: 'To Winner',
+  onclick: () => {
+    appMain.replaceChildren(winnerSection);
+  },
 });
 
 export const header = createElement('header', {
   className: ['header'],
+  children: [toGarageBtn, toWinnerBtn],
 });
-
-header.append(toGarageBtn, toWinnerBtn);
---
+++ src/app/pages/winner/winner-section.ts
@@ -0,0 +1,5 @@
+import { createElement } from '@/app/utils/create-element';
+
+export const winnerSection = createElement('section', {
+  className: ['winner-section'],
+});
